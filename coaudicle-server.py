fc86bdbe5bbd3e5b60bd2a4ff86c637481bab51e
Spencer Salazar <spencer.salazar@gmail.com>
2014-08-06 04:25:14 -0700

    async response

--- coaudicle-server.py
+++ coaudicle-server.py
@@ -1,7 +1,8 @@
 
 from twisted.web import server, resource
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
 from twisted.python import log
+from twisted.web.server import NOT_DONE_YET
 
 import types, uuid, json, cgi, datetime
 import ago # pip install ago
@@ -9,12 +10,7 @@ import ago # pip install ago
 class Site(server.Site):
     def getResourceFor(self, request):
         request.setHeader('Content-Type', 'application/json')
-        try:
-            return server.Site.getResourceFor(self, request)
-        except:
-            log.err('Processing request:')
-            log.err(request)
-            log.err()
+        return server.Site.getResourceFor(self, request)
 
 # shorthand convenience method
 bind = types.MethodType
@@ -52,6 +48,7 @@ class Room(resource.Resource):
             uuid = uuid.uuid4().hex
         self._uuid = uuid
         self._actions = []
+        self._defers = []
         
         # join action
         @bindPOST(self, 'join')
@@ -98,15 +95,38 @@ class Room(resource.Resource):
         @bindGET(self, 'actions')
         def actions_GET(self, request):            
             if 'after' in request.args:
-                last = int(request.args['after'][0])
-                return json.dumps(self._actions[last+1:])
+                after = int(request.args['after'][0])
+                if after+1 < len(self._actions):
+                    return self.renderActions(request, after)
+                else:
+                    d = defer.Deferred()
+                    d.request = request
+                    d.addCallback(self.renderActions, after, True)
+                    self._defers.append(d)
+                    
+                    return NOT_DONE_YET
                 
             else:
-                return json.dumps(self._actions)
+                return self.renderActions(request)
+    
+    def renderActions(self, request, after=None, async=False):
+        if after is not None:
+            data = json.dumps(self._actions[after+1:])
+        else:
+            data = json.dumps(self._actions)
+        
+        if async:
+            request.write(data)
+            request.finish()
+        else:
+            return data
     
     def postAction(self, action):
         action['aid'] = len(self._actions)
         self._actions.append(action)
+        for defer in self._defers:
+            defer.callback(defer.request)
+        del self._defers[:]
     
     def isMember(self, user_id):
         for member in self._members:
